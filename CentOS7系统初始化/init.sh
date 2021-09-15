e339f4e07f106571d6106e2299edaf818be08288
su <esc.9891@gmail.com>
2021-09-15 11:47:52 +0800

    Update init.sh

--- CentOS7系统初始化/init.sh
+++ CentOS7系统初始化/init.sh
@@ -14,12 +14,14 @@ cat << EOF
     [1]Set yum-repos to aliyun
     [2]Set yum-repos to tsinghua
     [3]Install epel-repos from aliyun
-    [4]Install basic utils, network utils and update system
-    [5]Set timezone and NTP
-    [6]Enable iptables and Disable firewalld
-    [7]Set vi=vim
-    [8]Set sysctl.conf
-    [9]Disable ipv6
+	[4]install mysql-repos from official
+    [5]Install basic utils, network utils and update system
+    [6]Set timezone and NTP
+    [7]Enable iptables and Disable firewalld
+    [8]Set vi=vim
+    [9]Set sysctl.conf
+    [10]Disable ipv6
+    [11]Disable selinux
     [a]Do all things(default)
     [0]Exit
 
@@ -38,30 +40,36 @@ case "$index" in
         ;;
     3)  d_epel
         ;;
-    4)  d_init
+	4)  d_mysql_repo
+	    ;;
+    5)  d_init
         ;;
-    5)  d_ntp
+    6)  d_ntp
         Echo_Green "Now the time is: "; date +%H:%M:%S
         ;;
-    6)  d_iptables
+    7)  d_iptables
         Echo_Green "The iptables rules: "; iptables -nvL
         ;;
-    7)  d_vi
+    8)  d_vi
         ;;
-    8)  d_sysctl
+    9)  d_sysctl
         ;;
-    9)  d_disable_ipv6
+    10) d_disable_ipv6
+        ;;
+    11) d_disable_selinux
         ;;
     0)  exit 0
         ;;
-    *)  d_epel
-        t_yum
+    *)  d_yum
+        d_epel
+		d_mysql_repo
         d_init
         d_ntp
         d_iptables
         d_vi
         d_sysctl
         d_disable_ipv6
+		d_disable_selinux
         Echo_Green "Your repolist: "; yum repolist
         Echo_Green "Now the time is: "; date +%H:%M:%S
         Echo_Green "The iptables rules: "; iptables -nvL
